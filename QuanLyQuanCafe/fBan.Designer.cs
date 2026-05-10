71a82f54c769e4f2cf0422b9aae02804ca8bfa22
ghostorya3 <88933306+ghostorya3@users.noreply.github.com>
2026-05-11 00:20:22 +0700

    Add files via upload

---
+++ QuanLyQuanCafe/fBan.Designer.cs
@@ -0,0 +1,235 @@
+﻿namespace QuanLyQuanCafe
+{
+    partial class fBan
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.dgvTables = new System.Windows.Forms.DataGridView();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.comboBox3 = new System.Windows.Forms.ComboBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvTables)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(783, 60);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(358, 55);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "QUẢN LÝ BÀN";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+           
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(1663, 1161);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(107, 54);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Đóng";
+            this.button1.UseVisualStyleBackColor = true;
+           
+            // 
+            // dgvTables
+            // 
+            this.dgvTables.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvTables.Location = new System.Drawing.Point(156, 561);
+            this.dgvTables.Name = "dgvTables";
+            this.dgvTables.Size = new System.Drawing.Size(1614, 526);
+            this.dgvTables.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(167, 272);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(79, 24);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Mã bàn";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(167, 327);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(88, 24);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Tên bàn";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(167, 376);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(145, 24);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Trạng thái bàn";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(167, 433);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(175, 24);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "Đã đặt trước bàn?";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(365, 276);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(501, 20);
+            this.textBox1.TabIndex = 9;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(365, 330);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(492, 20);
+            this.textBox2.TabIndex = 10;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(365, 381);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(486, 21);
+            this.comboBox2.TabIndex = 11;
+            // 
+            // comboBox3
+            // 
+            this.comboBox3.FormattingEnabled = true;
+            this.comboBox3.Location = new System.Drawing.Point(372, 439);
+            this.comboBox3.Name = "comboBox3";
+            this.comboBox3.Size = new System.Drawing.Size(478, 21);
+            this.comboBox3.TabIndex = 12;
+            
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(172, 1155);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(153, 59);
+            this.button2.TabIndex = 13;
+            this.button2.Text = "Xem thông tin";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(532, 1158);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(189, 56);
+            this.button3.TabIndex = 14;
+            this.button3.Text = "Lưu thông tin";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(917, 1155);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(183, 58);
+            this.button4.TabIndex = 15;
+            this.button4.Text = "Xóa bàn";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(1298, 1155);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(166, 58);
+            this.button5.TabIndex = 16;
+            this.button5.Text = "Thêm bàn";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // fBan
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Turquoise;
+            this.ClientSize = new System.Drawing.Size(1941, 1287);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.comboBox3);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.dgvTables);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Name = "fBan";
+            this.Text = "fBan";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvTables)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dgvTables;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.ComboBox comboBox3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+    }
+}
+
---
+++ QuanLyQuanCafe/fBan.cs
@@ -0,0 +1,386 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+
+namespace QuanLyQuanCafe
+{
+    public partial class fBan : Form
+    {
+        private string apiUrl =
+            "https://6a00a85436fb6ad04de05cca.mockapi.io/fBan";
+
+        private List<CafeTable> allTables = new List<CafeTable>();
+        private bool isSelecting = false;
+
+        public fBan()
+        {
+            InitializeComponent();
+            this.Load += async (s, e) => await LoadTables();
+            dgvTables.SelectionChanged += dgvTables_SelectionChanged;
+            button1.Click += (s, e) => Close();                            // button1 = Đóng
+            button2.Click += button2_Search_Click;                         // button2 = Tìm kiếm
+            button3.Click += async (s, e) => await button3_Update_Click(); // button3 = Cập nhật
+            button4.Click += async (s, e) => await button4_Delete_Click(); // button4 = Xóa
+            button5.Click += async (s, e) => await button5_Add_Click();    // button5 = Thêm mới
+
+            comboBox2.Items.AddRange(new string[] { "Đang sử dụng", "Trống" });
+            comboBox3.Items.AddRange(new string[] { "Đã đặt trước", "Chưa đặt" });
+        }
+
+        // ==================== MODEL ====================
+        public class CafeTable
+        {
+            public string id { get; set; }
+            public int tableid { get; set; }
+            public string tablename { get; set; }
+            public bool status { get; set; }
+            public bool reserved { get; set; }
+        }
+
+        // ==================== LOAD DỮ LIỆU ====================
+        private async Task LoadTables()
+        {
+            try
+            {
+                using (HttpClient client = new HttpClient())
+                {
+                    var response = await client.GetStringAsync(apiUrl);
+                    allTables = JsonConvert.DeserializeObject<List<CafeTable>>(response);
+                    dgvTables.DataSource = null;
+                    dgvTables.DataSource = allTables;
+                    CustomizeGrid();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("LỖI tải dữ liệu: " + ex.Message);
+            }
+        }
+
+        // ==================== CẤU HÌNH CỘT ====================
+        private void CustomizeGrid()
+        {
+            dgvTables.Columns["id"].HeaderText = "STT";
+            dgvTables.Columns["tableid"].HeaderText = "Mã bàn";
+            dgvTables.Columns["tablename"].HeaderText = "Tên bàn";
+            dgvTables.Columns["status"].HeaderText = "Trạng thái bàn";
+            dgvTables.Columns["reserved"].HeaderText = "Đã đặt trước?";
+        }
+
+        // ==================== CLICK HÀNG TRÊN GRID ====================
+        private void dgvTables_SelectionChanged(object sender, EventArgs e)
+        {
+            if (isSelecting) return;
+            if (dgvTables.SelectedRows.Count == 0) return;
+
+            CafeTable selected = dgvTables.SelectedRows[0].DataBoundItem as CafeTable;
+            if (selected == null) return;
+
+            isSelecting = true;
+            textBox1.Text = selected.tableid.ToString();
+            textBox2.Text = selected.tablename;
+            comboBox2.Text = selected.status ? "Đang sử dụng" : "Trống";
+            comboBox3.Text = selected.reserved ? "Đã đặt trước" : "Chưa đặt";
+            isSelecting = false;
+        }
+
+        // ==================== BUTTON2: SEARCH ====================
+        private void button2_Search_Click(object sender, EventArgs e)
+        {
+            string keyword = textBox1.Text.Trim();
+
+            if (string.IsNullOrEmpty(keyword))
+            {
+                MessageBox.Show("Vui lòng nhập mã bàn cần tìm!", "Thông báo",
+                    MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            CafeTable found = allTables.FirstOrDefault(t => t.tableid.ToString() == keyword);
+
+            if (found == null)
+            {
+                MessageBox.Show($"Không tìm thấy bàn có mã: {keyword}", "Không tìm thấy",
+                    MessageBoxButtons.OK, MessageBoxIcon.Information);
+                return;
+            }
+
+            textBox1.Text = found.tableid.ToString();
+            textBox2.Text = found.tablename;
+            comboBox2.Text = found.status ? "Đang sử dụng" : "Trống";
+            comboBox3.Text = found.reserved ? "Đã đặt trước" : "Chưa đặt";
+
+            foreach (DataGridViewRow row in dgvTables.Rows)
+            {
+                CafeTable item = row.DataBoundItem as CafeTable;
+                if (item != null && item.tableid.ToString() == keyword)
+                {
+                    dgvTables.ClearSelection();
+                    row.Selected = true;
+                    dgvTables.FirstDisplayedScrollingRowIndex = row.Index;
+                    break;
+                }
+            }
+        }
+
+        // ==================== BUTTON3: CẬP NHẬT ====================
+        private async Task button3_Update_Click()
+        {
+            string keyword = textBox1.Text.Trim();
+
+            if (string.IsNullOrEmpty(keyword))
+            {
+                MessageBox.Show("Vui lòng tìm bàn trước khi cập nhật!", "Thông báo",
+                    MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            CafeTable found = allTables.FirstOrDefault(t => t.tableid.ToString() == keyword);
+
+            if (found == null)
+            {
+                MessageBox.Show($"Không tìm thấy bàn có mã: {keyword}", "Không tìm thấy",
+                    MessageBoxButtons.OK, MessageBoxIcon.Information);
+                return;
+            }
+
+            found.tablename = textBox2.Text.Trim();
+            found.status = comboBox2.Text == "Đang sử dụng";
+            found.reserved = comboBox3.Text == "Đã đặt trước";
+
+            bool success = await UpdateTableApi(found);
+
+            if (success)
+            {
+                dgvTables.DataSource = null;
+                dgvTables.DataSource = allTables;
+                CustomizeGrid();
+
+                foreach (DataGridViewRow row in dgvTables.Rows)
+                {
+                    CafeTable item = row.DataBoundItem as CafeTable;
+                    if (item != null && item.tableid.ToString() == keyword)
+                    {
+                        dgvTables.ClearSelection();
+                        row.Selected = true;
+                        dgvTables.FirstDisplayedScrollingRowIndex = row.Index;
+                        break;
+                    }
+                }
+
+                MessageBox.Show("Cập nhật thành công!", "Thông báo",
+                    MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+        // ==================== BUTTON4: XÓA ====================
+        private async Task button4_Delete_Click()
+        {
+            string keyword = textBox1.Text.Trim();
+
+            if (string.IsNullOrEmpty(keyword))
+            {
+                MessageBox.Show("Vui lòng tìm bàn trước khi xóa!", "Thông báo",
+                    MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            CafeTable found = allTables.FirstOrDefault(t => t.tableid.ToString() == keyword);
+
+            if (found == null)
+            {
+                MessageBox.Show($"Không tìm thấy bàn có mã: {keyword}", "Không tìm thấy",
+                    MessageBoxButtons.OK, MessageBoxIcon.Information);
+                return;
+            }
+
+            DialogResult confirm = MessageBox.Show(
+                $"Bạn có chắc muốn xóa bàn '{found.tablename}' không?",
+                "Xác nhận xóa",
+                MessageBoxButtons.YesNo,
+                MessageBoxIcon.Warning);
+
+            if (confirm != DialogResult.Yes) return;
+
+            bool success = await DeleteTableApi(found);
+
+            if (success)
+            {
+                allTables.Remove(found);
+                dgvTables.DataSource = null;
+                dgvTables.DataSource = allTables;
+                CustomizeGrid();
+
+                textBox1.Text = "";
+                textBox2.Text = "";
+                comboBox2.SelectedIndex = -1;
+                comboBox3.SelectedIndex = -1;
+
+                MessageBox.Show("Xóa thành công!", "Thông báo",
+                    MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+        // ==================== BUTTON5: THÊM MỚI ====================
+        private async Task button5_Add_Click()
+        {
+            string keyword = textBox1.Text.Trim();
+            string tablename = textBox2.Text.Trim();
+
+            if (string.IsNullOrEmpty(keyword))
+            {
+                MessageBox.Show("Vui lòng nhập mã bàn!", "Thông báo",
+                    MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            if (string.IsNullOrEmpty(tablename))
+            {
+                MessageBox.Show("Vui lòng nhập tên bàn!", "Thông báo",
+                    MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            if (!int.TryParse(keyword, out int tableid))
+            {
+                MessageBox.Show("Mã bàn phải là số!", "Thông báo",
+                    MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            // Kiểm tra trùng mã bàn
+            bool isDuplicate = allTables.Any(t => t.tableid == tableid);
+            if (isDuplicate)
+            {
+                MessageBox.Show($"Mã bàn {tableid} đã tồn tại! Vui lòng nhập mã khác.",
+                    "Trùng mã bàn",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Warning);
+                return;
+            }
+
+            CafeTable newTable = new CafeTable
+            {
+                tableid = tableid,
+                tablename = tablename,
+                status = comboBox2.Text == "Đang sử dụng",
+                reserved = comboBox3.Text == "Đã đặt trước"
+            };
+
+            bool success = await AddTableApi(newTable);
+
+            if (success)
+            {
+                await LoadTables();
+
+                foreach (DataGridViewRow row in dgvTables.Rows)
+                {
+                    CafeTable item = row.DataBoundItem as CafeTable;
+                    if (item != null && item.tableid == tableid)
+                    {
+                        dgvTables.ClearSelection();
+                        row.Selected = true;
+                        dgvTables.FirstDisplayedScrollingRowIndex = row.Index;
+                        break;
+                    }
+                }
+
+                MessageBox.Show("Thêm bàn thành công!", "Thông báo",
+                    MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+        // ==================== GỌI API POST ====================
+        private async Task<bool> AddTableApi(CafeTable table)
+        {
+            try
+            {
+                using (HttpClient client = new HttpClient())
+                {
+                    string json = JsonConvert.SerializeObject(table);
+                    StringContent content = new StringContent(json, Encoding.UTF8, "application/json");
+
+                    HttpResponseMessage response = await client.PostAsync(apiUrl, content);
+
+                    if (!response.IsSuccessStatusCode)
+                    {
+                        MessageBox.Show("Lỗi thêm API: " + response.StatusCode);
+                        return false;
+                    }
+
+                    return true;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("LỖI gọi API: " + ex.Message);
+                return false;
+            }
+        }
+
+        // ==================== GỌI API PUT ====================
+        private async Task<bool> UpdateTableApi(CafeTable table)
+        {
+            try
+            {
+                using (HttpClient client = new HttpClient())
+                {
+                    string json = JsonConvert.SerializeObject(table);
+                    StringContent content = new StringContent(json, Encoding.UTF8, "application/json");
+
+                    string url = $"{apiUrl}/{table.id}";
+                    HttpResponseMessage response = await client.PutAsync(url, content);
+
+                    if (!response.IsSuccessStatusCode)
+                    {
+                        MessageBox.Show("Lỗi cập nhật API: " + response.StatusCode);
+                        return false;
+                    }
+
+                    return true;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("LỖI gọi API: " + ex.Message);
+                return false;
+            }
+        }
+
+        // ==================== GỌI API DELETE ====================
+        private async Task<bool> DeleteTableApi(CafeTable table)
+        {
+            try
+            {
+                using (HttpClient client = new HttpClient())
+                {
+                    string url = $"{apiUrl}/{table.id}";
+                    HttpResponseMessage response = await client.DeleteAsync(url);
+
+                    if (!response.IsSuccessStatusCode)
+                    {
+                        MessageBox.Show("Lỗi xóa API: " + response.StatusCode);
+                        return false;
+                    }
+
+                    return true;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("LỖI gọi API: " + ex.Message);
+                return false;
+            }
+        }
+
+        // ==================== CÁC EVENT TRỐNG ====================
+        private void label1_Click(object sender, EventArgs e) { }
+        private void label2_Click(object sender, EventArgs e) { }
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e) { }
+    }
+}
